ee7eab48a544bc3cb699eb0281eb4a008282bf7a
Daniel Welsh <danjwelsh24@gmail.com>
2018-01-22 09:33:06 +0000

    DW: + Started a Vue project for the web interface

---
+++ webapp/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <title-bar></title-bar>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import TitleBar from './components/Title.vue'
+import bulma from 'bulma'
+export default {
+  name: 'App',
+  components: {
+    TitleBar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ webapp/src/components/Title.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+  <div class="title-bar">
+    <h1 class="title">Printer Pals</h1>
+    <h2 class="subtitle">Print and Play</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TitleBar'
+}
+</script>
+
+<style lang="scss">
+</style>
