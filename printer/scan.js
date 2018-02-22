d65ec41efc81d37b8260a6f1f540da1d9bbb7458
Daniel Welsh <danjwelsh24@gmail.com>
2018-02-22 10:54:02 +0000

    DW: + Scanner should query database after reading QR code

--- printer/scan.js
+++ printer/scan.js
@@ -1,41 +1,83 @@
-/*
+/**
  Read from stdin and print tasks from QR codes
  Date: 19/02/18
  Author: Daniel Welsh
  */
-const Raspistill = require('node-raspistill').Raspistill
-const camera = new Raspistill()
-const Jimp = require('jimp')
-const QrCode = require('qrcode-reader')
-const qr = new QrCode()
-const mongoose = require('mongoose')
-const schemas = require('./../web/schemas')
-let db = {}
 
+const Raspistill = require('node-raspistill').Raspistill // Take a photo using the camera module
+const camera = new Raspistill()
+const Jimp = require('jimp') // Javascript image processing
+const QrCode = require('qrcode-reader') // Read QR codes
+const qr = new QrCode() // New instance of a QR code
+const mongoose = require('mongoose') // MongoDB adapter
+const schemas = require('./../web/schemas') // Schemas for adapter
+let db = {} // Database object adapter connects to
+let isPrinting = false // Flag to skip printing if the printer is busy
+
+// Connect to mongo
 mongoose.connect(process.env.MONGO_URI)
+
+/**
+ * When a connection to the database is open assign the schemas to db.
+ */
 mongoose.connection.once('open', () => {
   db = schemas
 })
+
+/**
+ * Handle an error if one occurs when connecting the database
+ * TODO: actually handle this
+ */
 mongoose.connection.once('error', (error) => {
   console.error(error)
 })
 
+/**
+ * Callback for the QR code decoder
+ * - Checks the value and querys the database for a matching task.
+ * @param err
+ * @param value
+ * @returns {Promise<void>}
+ */
 qr.callback = async function (err, value) {
   if (err) {
     console.error(err)
     // TODO handle error
   }
+
+  let task
   console.log(value.result)
+  // Get ID from the QR code should be in the `id:<task_id>` format
   const id = value.result.split(':')[0]
+
+  // Fetch the task
   try {
-    const task = await db.schemas.Task.findOne({_id: id})
-    console.log(task)
+    task = await db.schemas.Task.findOne({_id: id})
   } catch (e) {
     console.error(e)
   }
+
+  // If the task exists, print it.
+  // TODO: Print the task.
+  if (task) {
+    isPrinting = true
+    console.log(task)
+  }
 }
 
-async function parse () {
+/**
+ * Decode a QR code
+ * - Takes a photo
+ * - Parses it to a bitmap using JIMP
+ * - Decodes it and querys database for task
+ * - Prints the task
+ * @returns {Promise<void>}
+ *
+ * TODO: Check whether this runs without JIMP
+ */
+async function decodeQR () {
+  if (isPrinting) return
+
   camera.takePhoto().then((photo) => {
     Jimp.read(photo).then((image) => {
       qr.decode(image.bitmap)
@@ -45,4 +87,5 @@ async function parse () {
   })
 }
 
-setInterval(parse, 1000)
+// Run the QR code decoder once a second
+setInterval(decodeQR, 1000)
