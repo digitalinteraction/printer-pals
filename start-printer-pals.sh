075938729ca3040852ea358f482440256bdf0ed1
Daniel Welsh <danjwelsh24@gmail.com>
2018-03-27 11:58:01 +0100

    DW: + Detach processes on start script

--- start-printer-pals.sh
+++ start-printer-pals.sh
@@ -3,8 +3,8 @@ mkdir -p ~/printer-pals/uploads
 mkdir -p ~/data/db
 chmod 775 ~/printer-pals/uploads
 
-mongod --storageEngine=mmapv1 --dbpath=~/data/db
+mongod --storageEngine=mmapv1 --dbpath=~/data/db &
 
 /etc/init.d/nginx start
 
-nodemon ~/printer-pals/web/app.js
+nodemon ~/printer-pals/web/app.js &
