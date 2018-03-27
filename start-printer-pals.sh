2772e7261100f818482abd8a25b644147b7cdfee
Daniel Welsh <danjwelsh24@gmail.com>
2018-03-27 13:34:13 +0100

    DW: + Changes to env paths to fix start up failure

--- start-printer-pals.sh
+++ start-printer-pals.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-mkdir -p ~/printer-pals/uploads
-mkdir -p ~/data/db
-chmod 775 ~/printer-pals/uploads
+mkdir -p /home/pi/printer-pals/uploads
+mkdir -p /home/pi/data/db
+chmod 775 /home/pi/printer-pals/uploads
 
-mongod --storageEngine=mmapv1 --dbpath=~/data/db &
+mongod --storageEngine=mmapv1 --dbpath=/home/pi/data/db &
 
 /etc/init.d/nginx start
 
-nodemon ~/printer-pals/web/app.js &
+nodemon /home/pi/printer-pals/web/app.js &
