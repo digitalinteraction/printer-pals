47485b1108bf08852c164ee98bf4c8b7d4850f14
Daniel Welsh <d.welsh@newcastle.ac.uk>
2018-03-29 15:54:49 +0100

    :fire: Cleaning up stock Vue files
    Deleted the stock HelloWorld files, and added some information to the page header

--- webapp/src/components/Title.vue
+++ webapp/src/components/Title.vue
@@ -3,8 +3,20 @@
     <div class="hero-body">
       <div class="container">
         <div class="title-bar is-large">
-          <h1 class="title">Printer Pals 🖨</h1>
-          <h2 class="subtitle">Print and Play</h2>
+          <div class="columns">
+            <div class="column is-two-fifths">
+              <h1 class="title">Printer Pals 🖨</h1>
+              <h2 class="subtitle">Print and Play</h2>
+              <p>
+                <strong>Let's play a game together! Use Printer Pals to upload a sound or a picture and add a task you can do together.</strong>
+                <br />
+                <br />
+                Upload a song and lyrics you can sing together 🎤
+                <br/>
+                Upload a picture of somewhere you recognise and a pop quiz 🏆
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
